0801d6d67b24aee7c2398ed39405557d160406cc
Evan Alford <evan.al4d@gmail.com>
2026-02-17 18:42:15 -0600

    added fetch call to propertyCard created getProperties function to fetch data

--- src/components/propertyCard.jsx
+++ src/components/propertyCard.jsx
@@ -1,9 +1,21 @@
-export const PropertyCard = ({ property }) => {
+export const PropertyCard = ({ property,updateCard }) => {
+    const handleDelete = () => { 
+        fetch(`http://localhost:8088/properties/${property.id}`, {
+            method: "DELETE",
+        }).then(() => {
+            updateCard()
+        })
+    }
+
     return (
         <section className="property-card">
             <h3>{property.name}</h3>
-            <p>City: {property.city}</p>
-            <p>Address: {property.address}</p>
+            <p>{property.address}</p>
+            <p>
+             {property.city}, {property.state}, {property.zip}
+            </p>
+
+            <button onClick={handleDelete}>Delete</button>
         </section>
     )
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,28 +2,36 @@ import { useEffect, useState } from "react"
 import { PropertyCard } from "../components/propertyCard"
 
 export const Dashboard = () => {
-
     const [properties, setProperties] = useState([])
+        
+    const getProperties = () => {
+            const loggedInUser = JSON.parse(localStorage.getItem("current_user"))
 
+            fetch(`http://localhost:8088/properties?userId=${loggedInUser.id}`)
+                .then((res) => res.json())
+                .then((propertiesArray) => {
+                    setProperties(propertiesArray)
+                })
+            
+    }
     useEffect(() => {
-        const loggedInUser = JSON.parse(localStorage.getItem("current_user"))
-    
-        fetch(`http://localhost:8088/properties?userId=${loggedInUser.id}`)
-            .then((res) => res.json())
-            .then((propertiesArray) => {
-                setProperties(propertiesArray)
-            })
-        },[])
+        getProperties()
+    }, [])
 
+        
         return (
             <div> 
-            <h1>Welcome to the Dashboard</h1>
-        {properties.map((property) => (
-            <PropertyCard
-            key={property.id}
-            property={property}
-            />
-        ))}
-        </div>
+                <h1>Welcome to the Dashboard</h1>
+                
+                {properties.map((property) => (
+                    <PropertyCard
+                    key={property.id}
+                    /*pass to property (each object in properties array) component as a prop*/
+                    property={property}
+                    /*pass the fetch function as a prop to ProperCard*/
+                    updateCard={getProperties}
+                    />
+                ))}
+            </div>
         )
     }
