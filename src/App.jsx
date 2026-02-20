0907b2bf5483a71288c84892da58cbdd72da81c4
Evan Alford <evan.al4d@gmail.com>
2026-02-20 11:17:45 -0600

    fix api ? added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { Login } from './pages/login'
 import { Dashboard } from './pages/dashboard.jsx'
 import { PropertyForm } from './pages/propertyForm.jsx'
 import { PropertyEdit } from './pages/propertyEdit.jsx'
+import { NavBar } from './components/navBar.jsx'
 
 function App() {
+const location = useLocation()
  
+const isLoggedIn = !!localStorage.getItem("current_user")
+const showNavBar = isLoggedIn && location.pathname !=="/"
+
 
   return (
-    
+    <>
+    {showNavBar ? <NavBar/> : null}
       <Routes>
         <Route path="/" element={<Login/>}/>
           <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path="properties/create" element={<PropertyForm/>} />
           <Route path="/edit/:propertyId" element={<PropertyEdit/>} />
       </Routes>
-    
+    </>
   )
 }
 
